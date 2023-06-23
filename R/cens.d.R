7cdcd133783ebbfc7df0f4178907814218e4584c
Mikis_Stasinopoulos@bitbucket.org <dmh.stasinopoulos@gmail.com>
2023-06-23 11:49:04 +0100

    new com

--- R/cens.d.R
+++ R/cens.d.R
@@ -1,37 +1,42 @@
+################################################################################
+################################################################################
+################################################################################
+################################################################################
 cens.d <- function(family = "NO", type = c( "right", "left", "interval"),...)
   {
     #if (!is.Surv(y)) stop(paste("the y variable is not a Surv object"))
      type <- match.arg(type)
     if (type=="counting") stop(paste("gamlss has no support for counting"))
    fname <- family
-    if (mode(family) != "character" && mode(family) != "name")
+if (mode(family) != "character" && mode(family) != "name")
    fname <- as.character(substitute(family))
  distype <- eval(call(family))$type
     dfun <- paste("d",fname,sep="")
     pfun <- paste("p",fname,sep="")
      pdf <- eval(parse(text=dfun))
      cdf <- eval(parse(text=pfun))
-fun <- if (type=="left")  
-       function(x, log = FALSE, ...)
+     fun <- if (type=="left")  
+        function(x, log = FALSE, ...)
         {
          if (!is.Surv(x)) stop(paste("the x variable is not a Surv object"))
         dfun <- ifelse(x[,"status"]==1, pdf(x[,1],log = TRUE,...),log(cdf(x[,1],...)))
         dfun <- if (log == TRUE) dfun else exp(dfun)
         dfun
-       }
-     else if (type=="right")
-      function(x, log = FALSE, ...)
+        }
+        else if (type=="right")
+        function(x, log = FALSE, ...)
        {
         if (!is.Surv(x)) stop(paste("the x variable is not a Surv object"))
         dfun <- ifelse(x[,"status"]==1, pdf(x[,1],log = TRUE,...),log(1-cdf(x[,1],...)))
         dfun <- if (log == TRUE) dfun else exp(dfun)
         dfun
        } 
-     else if (type=="interval")    
-      function(x, log = FALSE, ...)
+       else if (type=="interval")    
+       function(x, log = FALSE, ...)
        {
         if (!is.Surv(x)) stop(paste("the x variable is not a Surv object"))
-        dfun0 <-ifelse(x[,"status"]==0, cdf(x[,1], lower.tail=F, log.p=T, ...),0) # right  equivalent: log(1-cdf(y[,1],...)) cdf(y[,1], lower.tail=F, log.p=T, ...) 
+        dfun0 <-ifelse(x[,"status"]==0, cdf(x[,1], lower.tail=F, log.p=T, ...),0) 
+        # right  equivalent: log(1-cdf(y[,1],...)) cdf(y[,1], lower.tail=F, log.p=T, ...) 
         dfun1 <-ifelse(x[,"status"]==1, pdf(x[,1],log = TRUE,...),0)# death
         dfun2 <-ifelse(x[,"status"]==2, cdf(x[,1],  log.p=T, ...),0)# left  
         suppressWarnings(dfun3 <-ifelse(x[,"status"]==3, log(cdf(x[,2],...)-cdf(x[,1],...)),0))# interval
@@ -41,3 +46,7 @@ fun <- if (type=="left")
        }
   fun
   }
+################################################################################
+################################################################################
+################################################################################
+################################################################################
--- R/gen.cens.R
+++ R/gen.cens.R
@@ -1,41 +1,46 @@
+################################################################################
+################################################################################
+################################################################################
+################################################################################
 gen.cens <- function(family = "NO", 
                       name = "cens", 
                       type = c( "right", "left", "interval"), 
                       ...)
- {
-  type <- match.arg(type)
-   fam  <- as.gamlss.family(family) # family 
-    fname <- fam$family[[1]] # family name  
- # fname <- family
- # if (mode(family) != "character" && mode(family) != "name")
- # fname <- as.character(substitute(family))
-   dfun <- paste(paste("d",fname,sep=""), substr(type,start=1,stop=1),substr(name,start=1,stop=1), sep="") # say dNOrc
-   pfun <- paste(paste("p",fname,sep=""), substr(type,start=1,stop=1),substr(name,start=1,stop=1), sep="") # say pNOrc
-   qfun <- paste(paste("q",fname,sep=""), substr(type,start=1,stop=1),substr(name,start=1,stop=1), sep="") # say qNOrc
-#   dfun <- paste(paste("d",fname,sep=""), name, sep="")
-#   pfun <- paste(paste("p",fname,sep=""), name, sep="")
-#   qfun <- paste(paste("q",fname,sep=""), name, sep="")
-#   rfun <- paste(paste("r",fname,sep=""), name, sep="")
-    fun <- paste(fname, substr(type,start=1,stop=1),substr(name,start=1,stop=1), sep="")
-   alldislist <-c(dfun,pfun,qfun,fun)
-   # generate d 
-   eval(dummy <- cens.d(family = fname, type = type, ...))
-   eval(call("<-",as.name(dfun),dummy), envir=parent.frame(n = 1))
-   # generate p
-   eval(dummy <- cens.p(par, family = fname, type = type, ...))
-   eval(call("<-",as.name(pfun),dummy), envir=parent.frame(n = 1))
-   # generate q
-   eval(dummy <- cens.q(par, family = fname, type = type, ...))
-   eval(call("<-",as.name(qfun),dummy), envir=parent.frame(n = 1))   
-   # generate the fitting distribution
-   eval(dummy <- cens(family = fam, type = type, name=name, local=FALSE, ...))
-   eval(call("<-",as.name(fun),dummy), envir=parent.frame(n = 1))
-  cat("A censored family of distributions from",  fname, "has been generated \n", 
-  "and saved under the names: ", "\n",paste(alldislist, sep=","),"\n")#
+{
+      type <- match.arg(type)
+      fam  <- as.gamlss.family(family) # family 
+     fname <- fam$family[[1]] # family name  
+      dfun <- paste(paste("d",fname,sep=""), substr(type,start=1,stop=1),
+                   substr(name,start=1,stop=1), sep="") # say dNOrc
+      pfun <- paste(paste("p",fname,sep=""), substr(type,start=1,stop=1),
+                   substr(name,start=1,stop=1), sep="") # say pNOrc
+      qfun <- paste(paste("q",fname,sep=""), substr(type,start=1,stop=1),
+                  substr(name,start=1,stop=1), sep="") # say qNOrc
+       fun <- paste(fname, substr(type,start=1,stop=1),
+                   substr(name,start=1,stop=1), sep="")
+alldislist <- c(dfun,pfun,qfun,fun)
+# generate d 
+eval(dummy <- cens.d(family = fname, type = type, ...))
+eval(call("<-",as.name(dfun),dummy), envir=parent.frame(n = 1))
+# generate p
+eval(dummy <- cens.p(par, family = fname, type = type, ...))
+eval(call("<-",as.name(pfun),dummy), envir=parent.frame(n = 1))
+# generate q
+eval(dummy <- cens.q(par, family = fname, type = type, ...))
+eval(call("<-",as.name(qfun),dummy), envir=parent.frame(n = 1))   
+# generate the fitting distribution
+eval(dummy <- cens(family = fam, type = type, name=name, local=FALSE, ...))
+eval(call("<-",as.name(fun),dummy), envir=parent.frame(n = 1))
+  cat("A censored family of distributions from",  fname, 
+      "has been generated \n", "and saved under the names: ", 
+      "\n",paste(alldislist, sep=","),"\n")#
   cat("The type of censoring is", type,  " \n") 
  }
-#----------------------------------------------------------------------------------------
-# this is a dummy function to be able to generate a q function
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+# this is a dummy function to be able to generate a quantile function
 # needed if centiles are to be form from a given fitted model 
 cens.q <-function(family = "NO", ...)
   {
@@ -46,4 +51,7 @@ cens.q <-function(family = "NO", ...)
    invcdf <- eval(parse(text=qfun))
    invcdf
   }
-#----------------------------------------------------------------------------------------
+################################################################################
+################################################################################
+################################################################################
+################################################################################
